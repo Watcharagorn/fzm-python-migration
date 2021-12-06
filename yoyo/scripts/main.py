aa492b7713ea940eca26722c9ec3031ac424880b
Watcharagorn <firstw927@gmail.com>
2021-12-06 18:37:40 +0000

    fix: migration_table_prefix defaul value

--- yoyo/scripts/main.py
+++ yoyo/scripts/main.py
@@ -261,13 +261,7 @@ def get_backend(args, config):
         except configparser.NoOptionError:
             migration_table = default_migration_table
 
-    try:
-        migration_table_prefix = args.migration_table_prefix
-    except AttributeError:
-        try:
-            migration_table_prefix = config.get("DEFAULT", "migration_table_prefix")
-        except configparser.NoOptionError:
-            migration_table_prefix = ""
+    migration_table_prefix = args.migration_table_prefix | config.get("DEFAULT", "migration_table_prefix") | ""
 
     if dburi is None:
         raise InvalidArgument("Please specify a database uri")
